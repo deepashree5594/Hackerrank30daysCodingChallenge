66c183981b01f250be9669e765afb07d63aa5085
Deepashree <56852374+deepashree5594@users.noreply.github.com>
2019-11-06 12:00:12 -0800

    Add files via upload

---
+++ Day16/src/Solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+
+        try {
+            int i = Integer.parseInt(S);
+            System.out.println(i);
+        }
+        catch(Exception numberFormatException)
+        {
+            System.out.println("Bad String");
+        }
+
+    }
+}
+
